ecc69f4a6db186f8fe286c27dc74783f23bfd2ff
hope <hope@i-on.net>
2021-04-19 16:23:58 +0900

    next-basic setting

--- src/components/common/heading.tsx
+++ src/components/common/heading.tsx
@@ -7,15 +7,15 @@ const HeadingWrap = styled.div`
 `
 
 const StyledHeading = styled.div`
-    font-size: ${({level} : {level:number}) => 
+    font-size: ${({level} : {level: number}) => 
             +level === 2 ? '30px' :
                     +level === 3 ? '20px' : '40px' };
     font-weight: 500;
 `
 
-const Heading = ({level, title}) => (
+const Heading = ({level, title}: {level: number, title: string}) => (
     <HeadingWrap className="title">
-      <StyledHeading as={+level === 1 ? "h1" : +level === 2 ? "h2" : +level === 3 ? "h3" : "h4"} level={+level}>{title}</StyledHeading>
+      <StyledHeading as={level === 1 ? "h1" : level === 2 ? "h2" : level === 3 ? "h3" : "h4"} level={+level}>{title}</StyledHeading>
     </HeadingWrap>
 )
 
